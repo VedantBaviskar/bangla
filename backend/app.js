7fd776fb3f24629f726aba3256b8f01296d45369
unknown <baviskarvedant84@gmail.com>
2023-05-20 13:19:04 +0530

    04

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,17 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const path = require("path");
+const path = require("path");
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://bangla-frontend.vercel.app/',
+    origin: 'https://bangla-frontend.vercel.app',
     credentials: true
   })
 );
-app.use("/", express.static("uploads"));
-app.use("/", (req, res) => {
+app.use("/", express.static(path.join(__dirname,"./uploads")));
+app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
